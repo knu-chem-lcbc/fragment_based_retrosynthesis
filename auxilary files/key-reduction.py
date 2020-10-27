4965ee15e51f1242c226c9b392d12985202c0159
azpisruh <azpisruh@gmail.com>
2020-10-28 04:42:37 +0900

    test input gen

--- auxilary files/key-reduction.py
+++ auxilary files/key-reduction.py
@@ -10,7 +10,10 @@ parser.add_argument('fnameR', type = str)
 parser.add_argument('fnameW', type = str)
 args = parser.parse_args()
 
-assigned = [1, 2, 3, 4, 5, 6, 7, 9, 10, 12, 18, 20, 27, 29, 30, 31, 35, 42, 44, 46, 166, 14, 49, 63, 56, 59, 15, 39, 40, 48, 87, 103, 107, 134, 23, 21, 64, 125, 28, 70]
+assigned = [1, 2, 3, 4, 5, 6, 7, 9, 10, 12, 18, 20, 27, 29, 30, 31, 
+            35, 42, 44, 46, 166, 14, 49, 63, 56, 59, 15, 39, 40, 48, 
+            87, 103, 107, 134, 23, 21, 64, 125, 28, 70]
+
 with open(args.fnameR) as f:
     with open(args.fnameW, 'a') as p:
         for line in f.readlines():
